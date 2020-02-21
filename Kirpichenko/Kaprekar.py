dca93625789f0f7e2c3fe1763f33e451e4fb4ec0
Станислав <stasychbr@mail.ru>
2020-02-22 00:13:37 +0300

    Update Kaprekar.py

--- Kirpichenko/Kaprekar.py
+++ Kirpichenko/Kaprekar.py
@@ -1,3 +1,6 @@
+import time
+
+
 def dec(list_from_number, r):
     number = 0
     for k in range(len(list_from_number)):
@@ -19,9 +22,9 @@ def kaprekar(number, d, r):
         number_str = '0' + number_str
     list_from_number = [int(ch, r) for ch in number_str]
     list_from_number.sort()
-    min_num = dec(list_from_number, r) # int("".join(str(ch) for ch in list_from_number))
+    min_num = dec(list_from_number, r)
     list_from_number.reverse()
-    max_num = dec(list_from_number, r) # int("".join(str(ch) for ch in list_from_number))
+    max_num = dec(list_from_number, r)
     return convTo(max_num - min_num, r)
 
 
@@ -29,10 +32,38 @@ def findMagicNumbers(d, r):
     max_number = r ** d
     magic_numbers = []
     for i in range(max_number):
-        tmp = kaprekar(convTo(i, r), d, r)
-        if i == int(tmp, r):
+        i_str = convTo(i, r)
+        tmp = kaprekar(i_str, d, r)
+        if i_str == tmp:
             magic_numbers.append(tmp)
     return magic_numbers
 
 
-print(findMagicNumbers(3, 16))
+def findCycleLength(d, r):
+    length = 0
+    max_number = r ** d
+    for i in range(max_number):
+        cur_length = 0
+        list_cycle = []
+        i_str = convTo(i, r)
+        # print(i_str)
+        list_cycle.append(i_str)
+        last_num = kaprekar(i_str, d, r)
+        while last_num not in list_cycle:
+            cur_length += 1
+            list_cycle.append(last_num)
+            last_num = kaprekar(last_num, d, r)
+        if cur_length > length:
+            length = cur_length
+    return length
+
+    
+
+
+startTime = time.time()
+num_sys_lim = 16 + 1
+digits_lim = 6 + 1
+for i in range(1, digits_lim):
+    for j in range(2, num_sys_lim):
+        print(i, "digits", j, "system:", findCycleLength(i, j))
+print("--- %s sec. ---" % (time.time() - startTime))
