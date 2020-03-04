21e8017186f1f259ce5b55c67786fdcc3e0a1b08
Станислав <stasychbr@mail.ru>
2020-03-05 00:32:20 +0300

    added table with results

--- Kirpichenko/Kaprekar.py
+++ Kirpichenko/Kaprekar.py
@@ -1,4 +1,6 @@
 import time
+import cProfile
+# import pywin32
 
 
 def dec(list_from_number, r):
@@ -6,7 +8,7 @@ def dec(list_from_number, r):
     for k in range(len(list_from_number)):
         # number += list_from_number[len(list_from_number) - k - 1] * r ** k
         number *= r
-        number += list_from_number[k]
+        number += int(list_from_number[k], r)
     return number
 
 
@@ -21,8 +23,8 @@ def convTo(number, r):
 def kaprekar(number, d, r):
     number_str = str(number)
     number_str = number_str.rjust(d, '0')
-    list_from_number = [int(ch, r) for ch in number_str]
-    list_from_number.sort()
+    list_from_number = list(number_str)
+    list_from_number.sort(key=lambda ch: int(ch, r))
     min_num = dec(list_from_number, r)
     list_from_number.reverse()
     max_num = dec(list_from_number, r)
@@ -58,13 +60,22 @@ def findCycleLength(d, r):
             length = cur_length
     return length
 
-    
-
 
 startTime = time.time()
-num_sys_lim = 16 + 1
-digits_lim = 4 + 1
-for i in range(1, digits_lim):
-    for j in range(2, num_sys_lim):
-        print(i, "digits", j, "system:", findCycleLength(i, j), "|| magic numbers: ", findMagicNumbers(i, j))
+# pr = cProfile.Profile()
+# pr.enable()
+num_sys_lim = 10
+digits_lim = 6
+'''for i in range(1, digits_lim + 1):
+    for j in range(2, num_sys_lim + 1):
+        print(i, "digits", j, "system:", findCycleLength(i, j), "|| magic numbers: ", findMagicNumbers(i, j))'''
+file = open('table.csv', 'w')
+file.write('digits;system;max steps before magic number or cycle;magic numbers\n')
+for i in range(1, digits_lim + 1):
+    for j in range(2, num_sys_lim + 1):
+        file.write('%i;%i;%i;%s\n' % (i, j, findCycleLength(i, j), findMagicNumbers(i, j)))
+file.close()
+
+# pr.disable()
+# pr.print_stats()
 print("--- %s sec. ---" % (time.time() - startTime))
