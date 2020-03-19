562a6cb0eb56161008eff56c90b8ef7b9768def2
Tatiana655 <61006466+Tatiana655@users.noreply.github.com>
2020-03-19 17:13:57 +0500

    Update Symmetry.py

--- Iavoruk_Tatiana/Symmetry.py
+++ Iavoruk_Tatiana/Symmetry.py
@@ -1,3 +1,5 @@
+from itertools import *
+
 def check(num):
     l = len(num);
     for i in range(l//2):
@@ -21,24 +23,58 @@ def listToString(s):
         str1 += ele     
     return str1
 
-num = input();
-l = len(num);
-if (l % 2 == 0):
-    for i in range(l):
-        if (check(num) == 1):
-            print(num);
-        #print("before shi",num)
-        num = shift(list(num), 1);
-        num = listToString(num);
-        #print("after shi",num)
-else:
-    for i in range(l):
-        tmp = list(num).pop(0)
-        #print("before pop",num, tmp)
-        num = num[1:];
-        num = listToString(num);
-        #print("after pop", num)
-        if (check(num) == 1):
-            print(num);
-        num = num+str(tmp);
-        #num = listToString(num);
+def axisOfSymmetry(num):
+    l = len(num);
+    count = 0;
+    if (l % 2 == 0):
+        for i in range(l):
+            #для 0 узлов
+            if (check(num) == 1):
+                #print(num);
+                count+=1;
+            #для двух узлов    
+            #удалить два элемента проверить, потом добавить
+            if (i < l//2):
+                tmpf = list(num).pop(0);
+                #print('pop0',num)
+                tmpm = list(num).pop(l//2);
+                #print('pop l//2',num, l)
+                num = num[1:l//2]+ num[l//2+1:]
+#print(num)
+                if (check(num) == 1):
+                    #print(num);
+                    count+=1;
+                #print('tmpf ', tmpf,' ', num[0:l//2-1] ,' ', tmpm ,' ', num[l//2-1:l])
+                num = tmpf + num[0:l//2-1] + tmpm + num[l//2-1:l]
+            num = shift(list(num), 1);
+            num = listToString(num);
+    else:
+        for i in range(l):
+            #запомним первый элемент и удалим его, потом добавим
+            tmp = list(num).pop(0)
+            num = num[1:];
+            num = listToString(num);
+            if (check(num) == 1):
+                count+=1;
+                #print(num);
+            num = num+str(tmp);
+    #print(count);
+    return count
+
+n = int(input());
+k = int(input());
+num = set()
+str = "";
+str +='1'*k;
+str +='0'*(n-k);
+#print(str)
+for i in permutations(str, n):
+    f=0;
+    for j in range(n):
+        if (listToString(shift(list(i), j)) in num):
+            f=1;
+    if f==0:    
+        num.add(listToString(i))
+for i in num:
+    count = axisOfSymmetry(i);
+    print(i,' ', count)
