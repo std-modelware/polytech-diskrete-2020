9b4dbb03977fd4fc52cb8d5137a88f53a96f378f
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-03-27 20:28:24 +0300

    Update SymmetryAxis.py

--- Gerasimenko/SymmetryAxis.py
+++ Gerasimenko/SymmetryAxis.py
@@ -89,6 +89,32 @@ def Print(n, num, Trough_numbers, Between_numbers, Mixed_numbers):
 
     plot.show()
 
+    
+def Write(F, ele, Through_numbers, Between_numbers, Mixed_numbers):
+F.write(str(ele) + "\n")
+F.write("Through_numbers:\n")
+for i in Through_numbers:
+    F.write(str(i) + "\n")
+F.write("Between_number:\n")
+for i in Between_numbers:
+    F.write(str(i) + "\n")
+F.write("Mixed_numbers:\n")
+for i in Mixed_numbers:
+    F.write(str(i) + "\n")
+F.write("\n\n")
+
+
+def WriteDescription(F):
+F.write("Through_number обозначает оси симметрии, проходящие через цифры = вершины многоугольника.\n"
+        "В квадратных скобках числа в квадратных скобках обозначают номер цифр, через которые проходит ось\n"
+        "Between_numbers обозначает сои симметрии, проходящие между цифрами.\n"
+        "Допустим в квадратных скобках [n, m], значит, что ось симметрии проходит между"
+        "n и (n+1) цифрами и между m и (m+1) цифрами\n"
+        "Mixed_numbers обозначает оси симметрии, проходящие через цифру и между одновременно(в случае нечетного n)\n"
+        "Допусти в квадратных скобках [n, m], значит, что ось симметрии проходит"
+        "через n-ую цифру и между m и (m+1) цифрами\n\n\n")
+
+
 #Рассмотрим случай, когда ось симметрии проходит через два числа
 def AxisThroughNumbers(n, num):
     result = []
@@ -170,8 +196,12 @@ def SymmetryAxis(result, n):
     print(count)
 
 ###  MAIN  ###
-n = 18
-k = 9
-
+F = open('result.txt', 'w')
+print("Input n:")
+n = int(input());
+print("Input k:")
+k = int(input());
 result = BuiltNumbers(n, k)
-SymmetryAxis(result, n)
+WriteDescription(F)
+SymmetryAxis(result, n, F)
+F.close()
