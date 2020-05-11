24d045f5a55fdaafc0c4cff8e002ca08b9b48560
kirillkuks <60854701+kirillkuks@users.noreply.github.com>
2020-05-11 19:34:02 +0200

    Add files via upload

--- KuksenkoKirill/Bellman-Ford/Bellman-Ford.py
+++ KuksenkoKirill/Bellman-Ford/Bellman-Ford.py
@@ -8,7 +8,29 @@ def m_create(sz):
     return weight
 
 
-def all_edges(graph, p):
+def graph_read(file):
+    data = []
+    with open(file, 'r') as f:
+        for line in f:
+            data.append([int(x) for x in line.split()])
+    p = data[0][0]
+    q = data[0][1]
+    weight = m_create(p)
+    graph = {}
+    for i in range(1, q + 1):
+        v = data[i][0]
+        u = data[i][1]
+        w = data[i][2]
+        if v not in graph:
+            graph[v] = {u}
+        else:
+            graph[v].add(u)
+        weight[v][u] = w
+    s = data[q + 1][0]
+    return graph, p, q, weight, s
+
+
+def all_edges(graph):
     for v in graph:
         for u in graph[v]:
             yield v, u
@@ -30,40 +52,27 @@ def relax(v1, v2, predecessor, shortest_path, weight):
 def Bellman_Ford(graph, s, weight, p):
     predecessor, shortest_path = init(s, p)
     for i in range(0, p - 1):
-        for v, u in all_edges(graph, p):
+        for v, u in all_edges(graph):
             relax(u, v, predecessor, shortest_path, weight)
-    for v, u in all_edges(graph, p):
+    for v, u in all_edges(graph):
         if shortest_path[u] > shortest_path[v] + weight[v][u]:
             return False, False
     return predecessor, shortest_path
 
 
 def main():
-    p, q = [int(x) for x in input('Введите количество узлов и дуг - ').split()]
-    graph = {}
-    weight = m_create(p)
+    file = 'test2.txt'
 
-    print('Вводите начало, конец и вес дуги, нумерация узлов с нуля')
-    for i in range(0, q):
-        v, u, w = [int(x) for x in input().split()]
-        if v >= p or u >= p:
-            print('Неверные данные')
-            return
-        if v not in graph:
-            graph[v] = {u}
-        else:
-            graph[v].add(u)
-        weight[v][u] = w
-
-    s = int(input('Введите источник - '))
+    graph, p, q, weight, s = graph_read(file)
 
     predecessor, shortest_path = Bellman_Ford(graph, s, weight, p)
 
-    if predecessor is not False:
-        print('Вектор предшествования - ', predecessor)
-        print('Вектор длин путей - ', shortest_path)
-    else:
-        print('Граф содержит контур отрицательного веса')
+    with open('output2.txt', 'w') as f:
+        if predecessor is not False:
+            print('Вектор предшествования -', predecessor, file=f)
+            print('Вектор длин путей -', shortest_path, file=f)
+        else:
+            print('Граф содержит контур отрицательного веса', file=f)
 
 
 if __name__ == '__main__':
