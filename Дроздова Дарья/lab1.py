9917ab5ffcea7e1521e97557a972ecda3fe67608
Drozdova-Daria <60818854+Drozdova-Daria@users.noreply.github.com>
2020-05-12 10:31:26 +0300

    Update lab1.py

--- Дроздова Дарья/lab1.py
+++ Дроздова Дарья/lab1.py
@@ -1,34 +1,93 @@
-def kaprekar(number):
-    list_of_digits = [int(d) for d in str(number)]
-    list_of_digits1 = list_of_digits
-    n = len(list_of_digits)
-    if n < 4:
-        tmp = 4 - n
-        i = 0
-        while i < tmp:
-            list_of_digits.append(0)
-            i = i+1
-    list_of_digits.sort()
-    min = int("".join(map(str, list_of_digits)))
-    list_of_digits1.sort(reverse=True)
-    max = int("".join(map(str, list_of_digits1)))
-    print("%d -> min=%d, max=%d, -> %d" % (number, min, max, max - min))
-    return max - min
+def Conv (number, base):
+    newNum=[]
+    if number==0:
+        newNum.append(0)
+    else:
+        while number>=0:
+            newNum.append(number%base)
+            number //=base
+    newNum.reverse()
+    return newNum
 
+def Number(numberList, base):
+    n=len(numberList)
+    num=0
+    p=0;
+    for i in range(n-1, -1, -1):
+        num=num+numberList[i]*(base**p)
+        p=p+1
+    return num
 
-magicNumber = [6174, 0]
+magicNumber = []
 usedNumber = []
 
-from random import randint
+# Добавление нужного количества нулей в массив
+def AddZeroes (list, n, discharge):
+    tmp = discharge - n
+    i = 0
+    while i < tmp:
+        list.append(0)
+        i = i + 1
 
-number = randint(10, 100)
+def Subtraction (a, b, base):
+    A=Number(a, base)
+    B=Number(b, base)
+    c=B-A
+    result=Conv(c,base)
+    return result
 
-while True:
-    number = kaprekar(number)
-    if number in magicNumber:
-        print("magic number")
-        break
-    if number in usedNumber:
-        print("cycle")
-        break
-    usedNumber.append(number)
+def kaprekar(number, discharge, base):
+    list_of_digits = number
+    list_of_digits1 = list_of_digits
+    n = len(list_of_digits)
+    if n < discharge:
+        AddZeroes(list_of_digits, n, discharge)
+    #list_of_digits.sort()
+    min = sorted(list_of_digits)
+    max = sorted(list_of_digits1, reverse=True)
+    delta = Subtraction(min, max, base)
+    n = len(delta)
+    if n < discharge:
+        AddZeroes(delta, n, discharge)
+    delta.sort()
+    if delta == min:
+        delt = Number(delta, base)
+        if delt not in magicNumber:
+            magicNumber.append(delt)
+    return Subtraction(min, max, base)
+
+def Max (a, b):
+    if a>b:
+        return a
+    else:
+        return b
+
+for base in range (2,11):
+    for discharge in range (2,7):
+        max = 0
+        for number in range (0, base**discharge):
+            maxLenght = 0
+            numberList = Conv(number, base)
+            while True:
+                # print(number, "-> " , end=" ")
+                numberList = kaprekar(numberList, discharge, base)
+                maxLenght = maxLenght + 1
+                numberChange=Number(numberList, base)
+                if (numberChange in magicNumber) or (numberChange in usedNumber):
+                    # print(number);
+                    max=Max(max, maxLenght)
+                    break
+                usedNumber.append(numberChange)
+        print('Система счисления = ', base, end=' ')
+        print('Разряд = ', discharge, end=' ')
+        print('Максимальная длина = ', max, end=':')
+        for i in range(len(magicNumber)):
+            magicNumberList=Conv(magicNumber[i], base)
+            magicNumberString=''.join(map(str,magicNumberList))
+            print(magicNumberString, end=' ')
+        print()
+        magicNumber = [0]
+        usedNumber = []
+
+
+nd(number)
