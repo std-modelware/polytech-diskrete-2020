c582006b3ac03ebbe683ad3486ceac42f23c8ed7
Drusiand <58701926+Drusiand@users.noreply.github.com>
2020-05-15 13:37:29 +0300

    Update axes.py

--- Ivanov_Andrey/sym_axes/axes.py
+++ Ivanov_Andrey/sym_axes/axes.py
@@ -40,7 +40,21 @@ def get_not_equivalent(unique_arr):
             eq_arr.extend(get_all_equivalent(arr).copy())
     return non_eq_arr
 
+# Выделяем элемент, через который проходит ось симметрии
+def mark(elem):
+    return "[" + str(elem) + "]"
+
+def list2str(_list):
+    _str = ""
+    for elem in _list:
+        _str += str(elem)
+    return _str
+
 # Ищем оси симметрии
+# Изменение вывода: если ось проходит через какую-либо вершину, вершина отображается в квадратных скобках "[ ]"
+# Например: 0[1]2- ось проходит только через вершину 1, 0[1]2[3] - ось проходит через две вершины - 1 и 2
+# Если ось есть и при этом не проходит через вершины, то сторона, которую эта ось пересекает, обозначается прямой чертой "|"
+# Например: 12|34 - ось проходит через сторону, которая соединяет вершины 3 и 4
 def get_sym_axis(non_eq_arr):
     axis_count = 0
     eq_arr = get_all_equivalent(non_eq_arr);
@@ -50,7 +64,8 @@ def get_sym_axis(non_eq_arr):
             tmp.reverse()
             if tmp == arr:
                 non_eq_arr = tmp.copy()
-                print("\t", non_eq_arr, "-", end=' ')
+                non_eq_arr[len(arr) // 2] = mark(arr[len(arr) // 2])
+                print("\t", list2str(non_eq_arr), "-", end=' ')
                 print("Ось симметрии - прямая, проходящая через среднюю точку ", arr[len(arr) // 2])
                 axis_count += 1
     else:
@@ -61,7 +76,8 @@ def get_sym_axis(non_eq_arr):
             tmp.reverse()
             if tmp == arr:
                 non_eq_arr = tmp.copy()
-                print("\t\t", non_eq_arr, "-", end=' ')
+                non_eq_arr.insert(len(arr) // 2, "|")
+                print("\t\t", list2str(non_eq_arr), "-", end=' ')
                 print("Ось симметрии - прямая, проходящая между ", arr[len(arr) // 2 - 1], "и",
                       arr[len(arr) // 2])  # проходит через сторону, по обеим сторонам которой данные точки
                 axis_count += 1
@@ -72,7 +88,9 @@ def get_sym_axis(non_eq_arr):
             if tmp == tmp_arr:
                 tmp.append(vec_end)
                 non_eq_arr = tmp.copy()
-                print("\t\t", non_eq_arr, "-", end=' ')
+                non_eq_arr[len(tmp) // 2 - 1] = mark(arr[len(tmp) // 2 - 1])
+                non_eq_arr[len(tmp) - 1] = mark(arr[len(tmp) - 1])
+                print("\t\t", list2str(non_eq_arr), "-", end=' ')
                 print("Ось симметрии - прямая, проходящая через точки ", arr[len(tmp) // 2 - 1], "и",
                       arr[len(tmp) - 1])  # "разрезает напополам" данный правильный многоугольник
                 axis_count += 1
@@ -82,18 +100,18 @@ def get_sym_axis(non_eq_arr):
         print("\t\tОси симметрии отсутствуют")
     return axis_count
 
-n, m = input("Введите длину строки и количество единиц:\n").split()
-n = int(n)
-m = int(m)
-combinations = list(get_all_combinations(n, m))
-non_eq_combinations = (get_not_equivalent(combinations))
-print("Все возможные комбинации:")
-for comb in combinations:
-    print("\t", comb)
-print("Классы эквивалентности:")
-for non_eq_comb in non_eq_combinations:
-    print("\t", non_eq_comb)
-print("Оси симметрии:")
-for non_eq_comb in non_eq_combinations:
-    print("\tДля", non_eq_comb, ":")
-    get_sym_axis(non_eq_comb)
+for n in range(3, 10):
+    for m in range(1, n):
+        print("\nДлина строки", n, "с количеством единиц", m, ":")
+        combinations = list(get_all_combinations(n, m))
+        non_eq_combinations = (get_not_equivalent(combinations))
+        # print("Все возможные комбинации:")
+        # for comb in combinations:
+        #     print("\t", comb)
+        print("Классы эквивалентности:")
+        for non_eq_comb in non_eq_combinations:
+            print("\t", non_eq_comb)
+        print("Оси симметрии:")
+        for non_eq_comb in non_eq_combinations:
+            print("\tДля", non_eq_comb, ":")
+            get_sym_axis(non_eq_comb)
