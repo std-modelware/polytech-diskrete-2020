f0ea17f4e4da806d5e6d013eb9f7abad436ae11a
nelli-nn <60840392+nelli-nn@users.noreply.github.com>
2020-05-16 13:13:44 +0500

    Update lab1.py

--- Gallyamova Nelli/Kaprekar/lab1.py
+++ Gallyamova Nelli/Kaprekar/lab1.py
@@ -21,7 +21,6 @@ def convert_base(num, to_base, from_base):
 list_four = []
 listCycle = []
 
-
 def Kaprekar(num, digits, toRadix, fromRadix):
     count_cycle = 0
     used = []
@@ -63,7 +62,6 @@ with open(FILENAME, "w", newline="") as file:
     writer = csv.writer(file)
     file.write("digit/radix/max_cycle/magic numbers\n")
     for radix in range(2, 11):
-        t = 5453
         for digits in range(1, 7):
             nummm = ""
             num_before = []
@@ -81,13 +79,13 @@ with open(FILENAME, "w", newline="") as file:
     listCycle.clear()
     Cycles = []
     for j in range(0, list_len):
-        for number in range(1, 10000000):
+        digits = int(list_four[j][0])
+        for number in range(1, 10 ** digits):
             length = len(str(number))
             if list_four[j][3] == '0':
                 radix = 10
             else:
                 radix = int(list_four[j][2])
-            digits = int(list_four[j][0])
             if length > digits:
                 break
             num = Kaprekar(number, digits, radix, 10)
@@ -107,14 +105,10 @@ with open(FILENAME, "w", newline="") as file:
     for j in range(0, len(Cycles)):
         dataN.append([int(x) for x in Cycles[j].split()])
     for j in range(0, len(dataN)):
-        file.write(str(dataN[j][0]) + " ")
-        file.write(str(dataN[j][1]) + " ")
-        file.write(str(dataN[j][2]) + " " + ": ")
+        file.write(str(dataN[j][0]) + " " + str(dataN[j][1]) + " " + str(dataN[j][2]) + " " + ": ")
         str_magic = ""
         for i in range(3, len(dataN[j]) - 1):
             str_magic += str(dataN[j][i]) +", "
         str_magic += str(dataN[j][len(dataN[j]) - 1])
         file.write(str_magic + "\n")
 file.closed
-
-
